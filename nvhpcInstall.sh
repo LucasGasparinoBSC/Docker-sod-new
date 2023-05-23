c498f948b7376a24cac809c998653a219314069a
Lucas Gasparino <lucas.gasparino@bsc.es>
2023-05-23 17:25:47 +0200

    Now the nvhpc works, but thiss is installinng gcc13 which is bad. Later will trry to use older arch  base.

--- nvhpcInstall.sh
+++ nvhpcInstall.sh
@@ -7,10 +7,10 @@ export NVHPC_INSTALL_TYPE="single"
 
 ## Download the nvhpc installer
 wget https://developer.download.nvidia.com/hpc-sdk/23.3/nvhpc_2023_233_Linux_x86_64_cuda_multi.tar.gz
-tar -xvf nvhpc_2023_233_Linux_x86_64_cuda_12.0.tar.gz
+tar -xvf nvhpc_2023_233_Linux_x86_64_cuda_multi.tar.gz
 
 ## Run the installer
-/home/Apps/Compilers/nvidia/nvhpc_2023_233_Linux_x86_64_cuda_muulti/install
+/home/Apps/Compilers/nvidia/nvhpc_2023_233_Linux_x86_64_cuda_multi/install
 
 ## Cleanup
 rm -rf nvhpc_2023_233_Linux_x86_64_cuda_12.0.tar.gz nvhpc_2023_233_Linux_x86_64_cuda_multi *.tar.gz
